a56dc83e208f8605465dc6a012b9ff3d691c364a
sabalyshen <tedoldhead@gmail.com>
2021-07-18 21:41:35 +0800

    final

--- password.py
+++ password.py
@@ -1,13 +1,15 @@
 #這是密碼驗證程式
 password = 'a123456'
 t = 3
-while True:
+while t >= 1:
+	t = t - 1
 	pwd = input('請輸入密碼 :')
 	if pwd == password:
 		print('welcome')
 		break
 	else:
-		t = t - 1
-		print('剩餘', t,'次')
-		if t == 0:
-			break
+		print('密碼錯誤')
+		if t > 0:
+			print('剩餘', t, '次機會')
+		else:
+			print('沒機會了')

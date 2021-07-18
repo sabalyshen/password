d606e594f6e41c288fffb48184b40f21d117ae88
sabalyshen <tedoldhead@gmail.com>
2021-07-18 21:02:44 +0800

    second

--- password.py
+++ password.py
@@ -1,18 +1,13 @@
 #這是密碼驗證程式
-t = 0
+password = 'a123456'
+t = 3
 while True:
-	password = input('請輸入密碼 :')
-	if password == 'a123456':
+	pwd = input('請輸入密碼 :')
+	if pwd == password:
 		print('welcome')
 		break
 	else:
-		print('密碼錯誤')
-		t = int(t)
-		t = t + 1
-		if t == 3:
-			print('已經失敗3次了')
+		t = t - 1
+		print('剩餘', t,'次')
+		if t == 0:
 			break
-		elif t == 1:
-			print('已經失敗1次了')
-		elif t == 2:
-			print('已經失敗2次了')
